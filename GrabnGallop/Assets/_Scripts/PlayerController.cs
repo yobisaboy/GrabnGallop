d84ea427da7a93fb638dd6c6502f8cd96472a7e3
yoyohohoh <yho45@my.centennialcollege.ca>
2024-04-14 00:56:39 -0400

    Added player/ enemy enums, observer, singleton; turned controller to 2D movement

--- GrabnGallop/Assets/_Scripts/PlayerController.cs
+++ GrabnGallop/Assets/_Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.Scripting.APIUpdating;
 
@@ -12,8 +13,6 @@ public class PlayerController : MonoBehaviour
     #region Private Fields
     GrabnGallop _inputs;
     Vector2 _move;
-    Camera _camera;
-    Vector3 _camForward, _camRight;
     #endregion
 
     #region Serialize Fields
@@ -25,24 +24,11 @@ public class PlayerController : MonoBehaviour
     [SerializeField] float _gravity = -30.0f;
     [SerializeField] float _jumpHeight = 3.0f;
     [SerializeField] Vector3 _velocity;
-
-    [Header("Ground Detection")]
-    [SerializeField] Transform _groundCheck;
-    [SerializeField] float _groundRadius = 0.5f;
-    [SerializeField] LayerMask _groundMask;
-    [SerializeField] bool _isGrounded;
-
-    [Header("Respawn Locations")]
-    [SerializeField] Transform _respawn;
     #endregion
 
     void Awake()
     {
-        // Camera Control
-        _camera = Camera.main;
         _controller = GetComponent<CharacterController>();
-        
-        // Inputs
         OnEnable();
     }
 
@@ -58,42 +44,11 @@ public class PlayerController : MonoBehaviour
 
     void FixedUpdate()
     {   
-        // Camera Control
-        _camForward = _camera.transform.forward;
-        _camRight = _camera.transform.right;
-        _camForward.y = 0.0f;
-        _camRight.y = 0.0f;
-        _camForward.Normalize();
-        _camRight.Normalize();
-
-        // Player Ground
-        _isGrounded = Physics.CheckSphere(_groundCheck.position, _groundRadius, _groundMask);
-        if (_isGrounded && _velocity.y < 0.0f)
-        {
-            _velocity.y = -2.0f;
-        }
-
         // Player Movement
-        Vector3 movement = (_camRight * _move.x + _camForward * _move.y) * _speed * Time.fixedDeltaTime;
+        Vector3 moveDirection = new Vector3(_move.x, 0.0f, _move.y);
+        Vector3 movement = moveDirection * _speed * Time.fixedDeltaTime;
         if (!_controller.enabled) { return; }
         _controller.Move(movement);
-        _velocity.y += _gravity * Time.fixedDeltaTime;
-        _controller.Move(_velocity * Time.fixedDeltaTime);
-    }
-
-
-    void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(_groundCheck.position, _groundRadius);
-    }
-
-    void Jump()
-    {
-        if (_isGrounded)
-        {
-            _velocity.y = Mathf.Sqrt(_jumpHeight * -2.0f * _gravity);
-        }
     }
 
     private void OnTriggerEnter(Collider other)
